a174d33185353e8df50c7f11adfa27894224431c
ruantoninatto <ruantoninatto@gmail.com>
2022-04-04 21:22:06 -0300

    o que deu de fazer

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
+let contadorum = 0;
+let contadordois = 0;
+let contadortres = 0;
+let finalizador = document.querySelector(".botao-finalizar");
 function escolherprato (escolha) {
     
+        
    const clique = document.querySelector(".caixinha" + escolha);
-   const clicado = document.querySelector(".selecionada");
+      const clicado = document.querySelector(".selecionada");
    
    if (clicado !== null) {
-       clicado.classList.remove("selecionada");
-       
+       clicado.classList.remove("selecionada");       
    }   
    clique.classList.toggle("selecionada");
    
@@ -16,6 +20,15 @@ function escolherprato (escolha) {
        verificado.classList.remove("ligado");
    }
    verifique.classList.toggle("ligado");
+   if (contadorum < 1) { contadorum ++ };
+   let contador = contadorum + contadordois + contadortres;
+   if (contador == 3) {
+    
+    finalizador.classList.add("ligar");
+   }
+   
+   
+   
    
 }
 function escolherbebida (segundaescolha) {
@@ -24,10 +37,11 @@ function escolherbebida (segundaescolha) {
     const segundoclicado = document.querySelector(".segundaselecionada");
     if (segundoclicado !== null) {
         segundoclicado.classList.remove("segundaselecionada");
+        
     }   
     segundoclique.classList.toggle("segundaselecionada");
 
-    clique.classList.toggle("selecionada");
+    
    
     const segundoverifique = document.querySelector(".v" + segundaescolha);
     const segundoverificado = document.querySelector(".segundoligado");
@@ -36,7 +50,17 @@ function escolherbebida (segundaescolha) {
         segundoverificado.classList.remove("segundoligado");
     }
     segundoverifique.classList.toggle("segundoligado");
-   
+    if (contadordois < 1) {contadordois ++};
+    let contador = contadorum + contadordois + contadortres;
+   if (contador == 3) {
+    
+
+    finalizador.classList.add("ligar");
+
+   }
+
+    let valorbebida = document.getElementById("preco" + segundaescolha).textContent;
+    
 }
 function escolhersobremesa (terceiraescolha) {
     
@@ -44,6 +68,7 @@ function escolhersobremesa (terceiraescolha) {
     const terceiroclicado = document.querySelector(".terceiraselecionada");
     if (terceiroclicado !== null) {
         terceiroclicado.classList.remove("terceiraselecionada");
+      
     }   
     terceiroclique.classList.toggle("terceiraselecionada");
 
@@ -54,4 +79,27 @@ function escolhersobremesa (terceiraescolha) {
        terceiroverificado.classList.remove("terceiroligado");
    }
    terceiroverifique.classList.toggle("terceiroligado");
- }
+   if (contadortres < 1) {contadortres ++};
+   let contador = contadorum + contadordois + contadortres;
+   if (contador == 3) {
+    
+    finalizador.classList.add("ligar");
+   }
+
+   let valorsobremesa = document.getElementById("preco" + terceiraescolha).textContent;
+   
+    
+}
+
+       
+    
+
+    
+
+    
+   
+    
+
+ 
+     
+ 
